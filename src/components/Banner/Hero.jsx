39ba948f271ad8baaa73e0f8886efd00353bd8e8
Amitkumersarkar <amitekumersarkar@gmail.com>
2025-10-19 18:10:09 +0600

    banner section  updated

--- src/components/Banner/Hero.jsx
+++ src/components/Banner/Hero.jsx
@@ -10,7 +10,12 @@ const Hero = () => {
                 <p className="text-xs front-medium">Trusted by 10k+ people</p>
             </div>
             <h1 className="text-4xl sm:text-5xl md:text-6xl xl:text-[84px] font-medium xl-leading-[95px] max-w-5xl">Turning imagination into <span className="bg-gradient-to-r from-[#F527C5] to-[#4d8cea] bg-clip-text text-transparent">digital</span> impact.</h1>
+            <p className="text-sm sm:text-lg font-medium text-gray-500 dark:text-white/75 max-w-4/5 sm:max-w-lg pb-3">Creating meaningful connections and turning big ideas into interactive digital experiences.</p>
 
+            <div className="relative">
+                <img className="w-full max-w-6xl" src={assets.hero_img} alt="" />
+                <img className="absolute -top-40 -right-40 sm:-top-100 sm:-right-70 -z-1 dark:hidden" src={assets.bgImage1} alt="" />
+            </div>
         </div>
     );
 };
