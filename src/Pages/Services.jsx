884dad6f2b12cb32875d4e0a6bd502fec185be61
Amitkumersarkar <amitekumersarkar@gmail.com>
2025-10-19 19:53:10 +0600

    added serviceCard and fixes error

--- src/Pages/Services.jsx
+++ src/Pages/Services.jsx
@@ -1,4 +1,5 @@
 import assets from "../assets/assets";
+import ServiceCard from "./ServiceCard";
 import Title from "./Title";
 
 const Services = () => {
@@ -29,6 +30,12 @@ const Services = () => {
         <div id='services' className="relative flex flex-col items-center gap-7 px-4 sm:px-12 lg:px-24 xl:px-40 pt-30 text-gray-700 dark:text-white">
             <img className="absolute -top-110 -left-70 -z-1 dark:hidden" src={assets.bgImage2} alt="" />
             <Title title='How can we help?' description='From strategy to execution, we craft digital solutions that move your business forward.' ></Title>
+
+            <div className="flex flex-col md:grid grid-cols-2">
+                {
+                    servicesData.map((service, idx) => <ServiceCard key={idx} idx={idx} service={service}></ServiceCard>)
+                }
+            </div>
         </div>
     );
 };
