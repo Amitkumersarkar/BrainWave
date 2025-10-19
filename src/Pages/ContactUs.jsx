78f7c3c81cf03583161aebd1a0aede2fe664e572
Amitkumersarkar <amitekumersarkar@gmail.com>
2025-10-19 22:31:56 +0600

    web3 form validation done

--- src/Pages/ContactUs.jsx
+++ src/Pages/ContactUs.jsx
@@ -1,12 +1,37 @@
+import toast from "react-hot-toast";
 import assets from "../assets/assets";
 import Title from "./Title";
 
 const ContactUs = () => {
+    const submitForm = async (e) => {
+        e.preventDefault();
+        // setResult("Sending....");
+        const formData = new FormData(e.target);
 
+        formData.append("access_key", "274f14d3-b7c6-43ff-84f0-aa89e6ce64ba");
+        try {
+            const response = await fetch("https://api.web3forms.com/submit", {
+                method: "POST",
+                body: formData
+            });
+
+            const data = await response.json();
+
+            if (data.success) {
+                toast.success("Form Submitted Successfully");
+                e.target.reset();
+            } else {
+                toast.error(data.message)
+            }
+        } catch (error) {
+            toast.error(error.message)
+        }
+
+    }
     return (
         <div id='contact-us' className="flex flex-col items-center gap-7 px-4 sm:px-12 lg:px-24 xl:px-40 pt-30 text-gray-700 dark:text-white">
             <Title title='Reach out to us' description='From strategy to execution, we craft digital solutions that move your business forward.'></Title>
-            <form className="grid sm:grid-cols-2 gap-3 sm:gap-5 max-w-2xl w-full">
+            <form onSubmit={submitForm} className="grid sm:grid-cols-2 gap-3 sm:gap-5 max-w-2xl w-full">
                 <div>
                     <p className="mb-2 text-sm font-medium">Your Name</p>
                     <div className="flex pl-3 rounded-lg border border-gray-300 dark:border-gray-600">
@@ -23,9 +48,9 @@ const ContactUs = () => {
                 </div>
                 <div className="sm:col-span-2">
                     <p className="mb-2 text-sm font-medium">Message</p>
-                    <textarea name="textarea" id="" placeholder="Enter your text message" required rows="8" className="w-full p-3 text-sm outline-none rounded-lg border border-gray-300 dark:border-gray-600"></textarea>
+                    <textarea name="message" id="" placeholder="Enter your text message" required rows="8" className="w-full p-3 text-sm outline-none rounded-lg border border-gray-300 dark:border-gray-600"></textarea>
                 </div>
-                <button type="submit" className="w-max flex gap-2 bg-primary text-white text-sm px-10 py-3 font-semibold items-center rounded-full cursor-pointer hover:scale-105 transition-all" >Submit <img src={assets.arrow_icon} className="w-5" alt="" /></button>
+                <button type="submit" className="w-max flex gap-2 bg-primary text-white text-sm px-10 py-3 font-semibold items-center rounded-full cursor-pointer hover:scale-105 transition-all" >Submit <img src={assets.arrow_icon} className="w-4" alt="" /></button>
             </form>
         </div>
     );
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,12 +6,13 @@ import TrustedBy from "../../Pages/TrustedBy";
 import OurWork from "../../Pages/OurWork";
 import Team from "../../Pages/Team";
 import ContactUs from "../../Pages/ContactUs";
-
+import { Toaster } from "react-hot-toast";
 const Header = () => {
     const [theme, setTheme] = useState(localStorage.getItem('theme') ? localStorage.getItem('theme') : 'light');
 
     return (
         <div className="dark:bg-black relative">
+            <Toaster></Toaster>
             <Navbar theme={theme} setTheme={setTheme} />
             <Hero></Hero>
             <TrustedBy></TrustedBy>
